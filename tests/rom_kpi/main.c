6423148c0cc73f24c9cc73317abe70852e481530
Kyra Lengfeld <kyra.lengfeld@nordicsemi.no>
2025-06-02 14:47:39 +0200

    Rough draft of ROM KPI test.
    Signed-off-by: Kyra Lengfeld <kyra.lengfeld@nordicsemi.no>

---
+++ tests/rom_kpi/main.c
@@ -0,0 +1,9 @@
+#include <zephyr/ztest.h>
+
+ZTEST(rom_kpi, test_placeholder)
+{
+	// actual check happens post-build in Python
+	ztest_test_skip();
+}
+
+ZTEST_SUITE(rom_kpi, NULL, NULL, NULL, NULL, NULL);
