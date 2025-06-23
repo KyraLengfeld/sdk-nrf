dba4f5e510bd6c180eccf6e0b83b1abf64608920
Kyra Lengfeld <kyra.lengfeld@nordicsemi.no>
2025-06-23 09:05:59 +0200

    run rom report and minimal python
    Signed-off-by: Kyra Lengfeld <kyra.lengfeld@nordicsemi.no>

---
+++ tests/rom_kpi/check_rom_size.py
@@ -0,0 +1,22 @@
+# Copyright (c) 2025 Nordic Semiconductor ASA
+#
+# SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+import os
+import sys
+import json
+
+def find_identifier(symbol_tree, ident):
+    if symbol_tree['identifier'] == ident:
+        return symbol_tree
+    elif "children" in symbol_tree:
+        for c in symbol_tree["children"]:
+            if (found := find_identifier(c, ident)) is not None:
+                return found
+    return None
+
+if __name__ == "__main__":
+    print("Checking ROM size in", sys.argv[1])
+    with open(os.path.join(sys.argv[1], "rom.json")) as json_file:
+        report = json.load(json_file)
+        host_symbol = find_identifier(report['symbols'], 'subsys/bluetooth/host')
+        print(host_symbol["identifier"], host_symbol["size"])
